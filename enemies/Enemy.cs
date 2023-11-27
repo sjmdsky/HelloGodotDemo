ff3d069c9993ac89c30df028d9065e667061f22d
JeremySze <sjmdem@126.com>
2023-11-27 21:13:51 +0800

    fix: Direction init

--- enemies/Enemy.cs
+++ enemies/Enemy.cs
@@ -1,6 +1,5 @@
 using Godot;
-using System;
-
+using static Godot.GD;
 public enum Direction
 {
     Left = -1,
@@ -20,14 +19,16 @@ public partial class Enemy : CharacterBody2D
     [Export]
     public double Acceleration = 2000;
 
-    private Direction _direction = Direction.Left;
+    private Direction _direction;
     [Export]
     public Direction Direction
     {
         set
         {
             _direction = value;
-            graphics.Scale = _direction == Direction.Right ? new Vector2(-1, 1) : new Vector2(1, 1);
+            Print($"_direction -> {_direction}");
+            if (_direction != 0)
+                graphics.Scale = _direction == Direction.Right ? new Vector2(-1, 1) : new Vector2(1, 1);
         }
         get { return _direction; }
     }
@@ -38,5 +39,6 @@ public partial class Enemy : CharacterBody2D
         graphics = GetNode<Node2D>("Graphics");
         animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
         StateMachine = GetNode<StateMachine>("StateMachine");
+        Direction = Direction.Left;
     }
 }
